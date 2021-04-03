fbce092850475f9726f32aef2a7d5315348c7172
amandahuarng <amandahuarng@gmail.com>
2021-04-02 20:30:09 -0700

    first successful call from acs_script.py & added notes.md

--- acs_script.py
+++ acs_script.py
@@ -3,8 +3,19 @@ from dotenv import load_dotenv
 load_dotenv() 
 import os
 API_KEY = os.getenv('API_KEY')
-# GET "ACS DEMOGRAPHIC AND HOUSING ESTIMATES"
-#get = NAME, S0101_C01_001E & for = place: * & key = YOUR_KEY_GOES_HERE
-def get_population_data(city, year): 
-    base_url = "https://www.api.census.gov/data/{}/acs/acs5/subject?get=".format(year)
-    r = requests.get()
+# palm springs
+state = '06'
+place = '55254'
+def get_population_estimate(city, year): 
+    pop_est = 'B01001_001E' # according to 2019 variable list
+    total_population = 'https://api.census.gov/data/{}/acs/acs5?get=NAME,{}&for=place:{}&in=state:{}'.format(year, pop_est, city, state)
+    try:
+        r = requests.get(total_population)
+        print(r.json())
+        # [['NAME', 'B01001_001E', 'state', 'place'], ['Palm Springs city, California', '47897', '06', '55254']]
+    except:
+        print(total_population)
+        print("Connection refused by the server..")
+
+
+get_population_estimate(place, '2019')
---
+++ notes.md
@@ -0,0 +1,6 @@
+# 2019 acs/acs5 resources
+2019 Variable List
+* https://api.census.gov/data/2019/acs/acs5/variables.html
+
+2019 Example Calls List
+* https://api.census.gov/data/2019/acs/acs5/examples.html
