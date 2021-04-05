3e75228b7284e8ec9d56865fd23e1679ee0776a9
amandahuarng <amandahuarng@gmail.com>
2021-04-04 17:17:15 -0700

    added resources

--- acs_script.py
+++ acs_script.py
@@ -5,11 +5,11 @@ import os
 API_KEY = os.getenv('API_KEY')
 STATE = '06' # California state geoID
 PALM_SPRINGS = '55254'
-BASE_URL = 'https://api.census.gov/data/2019/acs/acs5?get=NAME,'
+BASE_URL = 'https://api.census.gov/data/2019/acs/acs5'
 
 def get_population_estimate(city): 
     pop_est = 'B01001_001E' # according to 2019 variable list
-    total_population = BASE_URL + '{}&for=place:{}&in=state:{}'.format(pop_est, city, STATE)
+    total_population = BASE_URL + '?get=NAME,{}&for=place:{}&in=state:{}'.format(pop_est, city, STATE)
     try:
         r = requests.get(total_population)
         print(r.json())
@@ -19,4 +19,17 @@ def get_population_estimate(city):
         print("Connection refused by the server..")
 
 
-get_population_estimate(PALM_SPRINGS)
+def aggregate_age_groups(city):
+    male_under_5 = 'B01001'
+    temp = BASE_URL + '?get=NAME,{}&for=place:{}&in=state:{}&key={}'.format(male_under_5, city, STATE, API_KEY)
+    try:
+        r = requests.get(temp)
+        print(r.html())
+        # [['NAME', 'B01001_001E', 'state', 'place'], ['Palm Springs city, California', '47897', '06', '55254']]
+    except:
+        print(temp)
+        print("Connection refused by the server..")
+
+#get_population_estimate(PALM_SPRINGS)
+
+aggregate_age_groups(PALM_SPRINGS)
--- notes.md
+++ notes.md
@@ -8,11 +8,17 @@
 Geoidentifiers:
 * https://www.census.gov/programs-surveys/geography/guidance/geo-identifiers.html
 
-Helpful Intro:
+Resources for getting started with the API:
 * https://www.census.gov/content/dam/Census/programs-surveys/acs/guidance/training-presentations/20180614_API.pdf
+* https://www.census.gov/content/dam/Census/data/developers/api-user-guide/api-guide.pdf
 
 Notes on Variable Formatting:
 * https://www.census.gov/data/developers/data-sets/acs-1year/notes-on-acs-api-variable-formats.html
 
 Notes on Variable Types:
 * https://www.census.gov/data/developers/data-sets/acs-1year/notes-on-acs-api-variable-types.html
+
+| Attribute   | ACS variable name |
+| ----------- | ----------- |
+| Population  | B01001_001E |
+| Age         |         |
