4d6d477f41d613bc4aee791a576a4d31575d4361
AbhiUppal <30747607+AbhiUppal@users.noreply.github.com>
2021-04-09 17:27:38 -0700

    add small note about lxml

--- acs_var_scrape.py
+++ acs_var_scrape.py
@@ -3,6 +3,7 @@ import requests
 
 with open("acs_var_list_5_year_2019.html") as html_file:
     # Do NOT print the contents of this. The html file is > 135k lines long.
+    # You may have to type pip3 install lxml
     soup = BeautifulSoup(html_file, "lxml")
 
 table = soup.find("tbody")
