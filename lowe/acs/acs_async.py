c0809f7fdb6fe76a46299b77a9f657bf8047d2fd
Abhi Uppal <abhinuv2000@gmail.com>
2021-08-12 17:00:37 -0700

    rewrite ACS async wrapper in object oriented paradigm

--- lowe/acs/acs_async.py
+++ lowe/acs/acs_async.py
@@ -8,7 +8,90 @@ from aiolimiter import AsyncLimiter
 from dotenv import load_dotenv
 from typing import Union, List
 
-load_dotenv()
+# @ext:njpwerner.autodocstring
+# https://marcobelo.medium.com/setting-up-python-black-on-visual-studio-code-5318eba4cd00
+
+
+class ACSClient(object):
+    def __init__(self, key_env_name: str = "API_KEY_ACS"):
+        """the ACS Client class provides methods for wrapping around the ACS client
+
+        Parameters
+        ----------
+        key_env_name : str, optional
+            name of the environment variable in your .env file corresponding to your ACS API key, by default "API_KEY_ACS"
+        """
+        load_dotenv()
+        self.API_KEY = os.environ.get(key_env_name, None)
+        try:
+            assert self.key is not None
+        except AssertionError:
+            print(
+                f"Error: please make sure you have your ACS API key loaded as an environment variable under the name {key_env_name}."
+            )
+
+    async def initialize(self):
+        self.session = await aiohttp.ClientSession()
+
+    async def close(self):
+        if not self.session.closed:
+            await self.session.close()
+
+    def _base_uri(self, year: Union[int, str], is_subject: bool = True):
+        base = f"https://api.census.gov/data/{str(year)}/acs/acs5"
+        return base if not is_subject else base + "/subject"
+
+    async def _collect_subject_table(
+        self,
+        tableid: str,
+        year: Union[int, str],
+        city_geoid: str,
+        state: str = "06",
+        is_subject: bool = True,
+    ):
+        """_collect_subject_table is a helper function that wraps a request to the ACS API
+
+        Parameters
+        ----------
+        tableid : str
+            [description]
+        year : Union[int, str]
+            [description]
+        city_geoid : str
+            [description]
+        state : str, optional
+            [description], by default "06"
+        is_subject : bool, optional
+            [description], by default True
+
+        Returns
+        -------
+        [type]
+            [description]
+        """
+        # TODO: Add functionality to lookup state codes from 2-character state codes (i.e. CA <--> 06)
+        # Check to see if the client session exists
+        try:
+            assert self.session is not None
+        except AssertionError:
+            print("Error: Please initialize client session with `client.initialize()`")
+
+        base = self._base_uri(year=year, is_subject=is_subject)
+
+        params = {
+            "get": f"group({tableid})",
+            "for": f"place:{city_geoid}",
+            "in": f"state:{state}",
+            "key": self.API_KEY,
+        }
+
+        if not is_subject:
+            params["get"] = tableid + ","
+
+        async with self.session as sesh:
+            resp = await sesh.get(base, params=params)
+            return await resp.json()
+
 
 API_KEY = os.environ.get("API_KEY_ACS", None)
 STATE = "06"  # California state geoID
@@ -19,21 +102,7 @@ pop_subj = "S0101"
 
 geoids = pd.read_csv("geoids_clean.csv")
 
+
 def _base_uri_subject(year: Union[str, int]):
     """Create a base URL for a subject table API call"""
     return f"https://api.census.gov/data/{year}/acs/acs5/subject"
-
-async def _collect_subject_table(session : aiohttp.ClientSession, subject: str, year: str, city_geoid : str, state : str = "06", api_key : str = API_KEY):
-    uri = _base_uri_subject(year=year)
-
-    params = {
-        "get" : f"group({subject})",
-        "for" : f"place:{city_geoid}",
-        "in" : f"state:{state}",
-        "key": api_key
-    }
-
-    async with session.get(uri, params=params) as r:
-        resp = await r.json()
-
-    return {year : resp}
