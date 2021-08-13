fc055c3f99bc46bbb81888b6434bd256c47fbd27
Abhi Uppal <abhinuv2000@gmail.com>
2021-08-13 14:02:05 -0700

    add functionality for arbitrary location requests

--- lowe/acs/acs_async.py
+++ lowe/acs/acs_async.py
@@ -6,10 +6,17 @@ import pandas as pd
 
 from aiolimiter import AsyncLimiter
 from dotenv import load_dotenv
-from typing import Union, List
+from typing import Union, List, Dict
 
 # @ext:njpwerner.autodocstring
 # https://marcobelo.medium.com/setting-up-python-black-on-visual-studio-code-5318eba4cd00
+
+# State -- first two digits of city geoid (state=*)
+# MSA - geocomp (MSA code, state)
+# County -- where to find the codes???? (county, state)
+# Place (city) -- (geoid,state) "place="
+
+# TODO: Implement backoff for retries
 
 
 class ACSClient(object):
@@ -24,20 +31,21 @@ class ACSClient(object):
         load_dotenv()
         self.API_KEY = os.environ.get(key_env_name, None)
         try:
-            assert self.key is not None
+            assert self.API_KEY is not None
         except AssertionError:
             print(
                 f"Error: make sure you have your ACS API key loaded as an environment variable under the name {key_env_name}."
             )
 
     async def initialize(self):
-        self.session = await aiohttp.ClientSession()
+        self.session = aiohttp.ClientSession()
 
     async def close(self):
         if not self.session.closed:
             await self.session.close()
 
     def _base_uri(self, year: Union[int, str], is_subject: bool = True):
+        # TODO: Add functionality for other kinds of estimates (non 5-year)
         base = f"https://api.census.gov/data/{str(year)}/acs/acs5"
         return base if not is_subject else base + "/subject"
 
@@ -45,11 +53,11 @@ class ACSClient(object):
         self,
         tableid: str,
         year: Union[int, str],
-        city_geoid: str,
-        state: str = "06",
+        location: Dict[str, str],
         is_subject: bool = True,
+        debug: bool = False,
     ):
-        """_collect_subject_table is a helper function that wraps a request to the ACS API
+        """_collect_subject_table is a helper function that wraps a GET request to the ACS API
 
         Parameters
         ----------
@@ -79,9 +87,23 @@ class ACSClient(object):
 
         base = self._base_uri(year=year, is_subject=is_subject)
 
+        state = location.pop("state")
+
+        key_translations = {"msa": "geocomp", "city": "place", "county": "county"}
+
+        # The 'for' part is a little more tricky. We need to append MSA, county, and city in that order, with %20 in between
+        place = ""
+        for k, v in location.items():
+            if v is not None:
+                place += (
+                    f"%20{key_translations[k]}:{v}"
+                    if len(place) > 0
+                    else f"{key_translations[k]}:{v}"
+                )
+
         params = {
             "get": f"group({tableid})",
-            "for": f"place:{city_geoid}",
+            "for": place,
             "in": f"state:{state}",
             "key": self.API_KEY,
         }
@@ -90,32 +112,39 @@ class ACSClient(object):
             params["get"] = tableid + ","
 
         async with self.session as sesh:
-            resp = await sesh.get(base, params=params)
+            resp = await sesh.get(base, params=params, raise_for_status=True)
+            if debug:
+                print(resp.url)
         return await resp.json()
 
     async def _process_request(
         self,
         tableid: str,
         year: Union[int, str],
-        city_geoid: str,
-        state: str = "06",
+        location: Dict[str, str],
         is_subject: bool = True,
         varfile: str = "acs_subjects_2019.json",
+        debug: bool = False,
     ):
         # Pulls data from ACS
+        print("making request...")
         resp = await self._collect_subject_table(
             tableid=tableid,
             year=year,
-            city_geoid=city_geoid,
-            state=state,
+            location=location,
             is_subject=is_subject,
         )
+
+        if debug:
+            print(resp.url)
         # Opens the JSON file with subject tables info
+        print("opening JSON...")
         with open(varfile) as f:
             subjectDict = json.load(f)
 
         # ids: list of subject ids
         # vals: list of corresponding values
+        print("post-processing....")
         ids, vals = resp[0], resp[1]
         concept_label = []
         values = []
@@ -175,5 +204,38 @@ class ACSClient(object):
 
         return res
 
-    async def get_acs(vars: List[str], year_start: str, year_end: str, city_geoid: Union[int, str], state: str, is_subject: bool = True):
+    async def get_acs(
+        vars: List[str],
+        year_start: str,
+        year_end: str,
+        city_geoid: Union[int, str],
+        state: str,
+        is_subject: bool = True,
+    ):
+        # Split the vars into equal partitions
         pass
+
+
+async def main():
+    pop_subj = "S0101"
+    PALM_SPRINGS = "55254"
+    RANCHO_MIRAGE = "59500"
+    STATE = "06"
+
+    client = ACSClient()
+    await client.initialize()
+
+    loc = {"state": STATE, "city": PALM_SPRINGS}
+
+    test_resp = await client._collect_subject_table(
+        tableid=pop_subj, year="2019", location=loc, debug=True
+    )
+
+    print(test_resp)
+
+    await client.close()
+
+    return test_resp
+
+
+asyncio.run(main())
