1e40304a6a1cf9564d003886d4dd0a78c257250d
Abhi Uppal <abhinuv2000@gmail.com>
2021-08-17 00:29:14 -0700

    black reformat pt 1

--- lowe/acs/sub_vars_clean.py
+++ lowe/acs/sub_vars_clean.py
@@ -2,17 +2,19 @@ import requests
 import json
 import re
 
-response = requests.get('https://api.census.gov/data/2019/acs/acs5/subject/variables.json')
+response = requests.get(
+    "https://api.census.gov/data/2019/acs/acs5/subject/variables.json"
+)
 
 data = response.json()
 for v in data.values():
     for elem in v:
         val = v[elem]
-        val['label'] = re.sub("!!", " ", val['label'])
-        #print(val['label'])
+        val["label"] = re.sub("!!", " ", val["label"])
+        # print(val['label'])
 
 with open("acs_subjects_2019.json", "w") as output:
-    json.dump(data['variables'], output)
+    json.dump(data["variables"], output)
 
 """
 Output file format:
