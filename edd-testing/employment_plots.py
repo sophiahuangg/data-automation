2b65c40cbc8b1334dc703d680cf0aeb5b7b5a6db
KaranGoel1 <kgoel24@cmc.edu>
2021-10-07 00:27:21 -0700

    finish change in employment composition plots

--- edd-testing/employment_plots.py
+++ edd-testing/employment_plots.py
@@ -33,6 +33,31 @@ def avg_mthly_employ(city: str, filepath: str):
     empl_data.index = empl_data.index.strftime('%Y')
     plots(empl_data)
 
-avg_mthly_employ('Cathedral City', '../data-automation/data/CV_EMPL.csv')
-
-
+def unemployment_composition(city: str, filepath: str):
+    def filter_df(city: str, df):
+        '''
+        Filters df by city
+        '''
+        df = df[df['City'] == city]
+        return df
+    def plots(df):
+        '''
+        Plots data using plotly
+        '''
+        empl_data_plot = px.bar(df, x=df.index, y='Change in Employment Composition', labels={'index': 'Sector'}, text='Change in Employment Composition')
+        empl_data_plot.update_traces(textposition='outside')
+        empl_data_plot.show()
+        return empl_data_plot
+    empl_data = pd.read_csv(filepath, na_values=["***", ".", "NA"])
+    empl_data['DATE'] = empl_data['DATE'].str.pad(width=6, side="left", fillchar="0")
+    empl_data['DATE'] = pd.to_datetime(empl_data.DATE, format='%y-%b')
+    empl_data.set_index('DATE', inplace=True)
+    empl_data = empl_data.fillna(0)
+    empl_data = filter_df(city, empl_data)
+    empl_data = empl_data[empl_data.index.year > 2019]
+    empl_data = empl_data[empl_data.index.month == 3]
+    change_df = pd.DataFrame()
+    change_df.index = empl_data.columns[1:]
+    change_df['Change in Employment Composition'] = empl_data.loc['2020-03-01', 'Agriculture, Forestry, Fishing and Hunting':] - empl_data.loc['2021-03-01', 'Agriculture, Forestry, Fishing and Hunting':]
+    change_df = change_df[change_df['Change in Employment Composition'] != 0.0]
+    plots(change_df)
