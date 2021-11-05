0516bffaef0b6ffb65b0084a8e947b7a909697e6
KaranGoel1 <kgoel24@cmc.edu>
2021-11-05 15:59:50 -0700

    latest update

--- edd-testing/employment_plots.py
+++ edd-testing/employment_plots.py
@@ -51,7 +51,7 @@ def change_employment_composition(city: str, filepath: str):
         empl_data_plot.show()
         return empl_data_plot
     def consolidate_industries(df: pd.DataFrame):
-        """Returns a dataframe that has the 12 consolidated industries we want"""
+        '''Returns a dataframe that has the 12 consolidated industries we want'''
         df["Logistics"] = df["Wholesale Trade"] + df["Transportation and Warehousing"] + df["Utilities"]
         df["FIRE"] = df["Finance and Insurance"] + df["Real Estate and Rental and Leasing"]
         df["Professional and Business Services"] = df["Professional, Scientific, and Technical Services"] + df["Management of Companies and Enterprises"] + df["Administrative and Support and Waste Management and Remediation Services"]
@@ -103,7 +103,7 @@ def employment_composition(city: str, data_path: str):
         empl_data_plot.show()
         return empl_data_plot
     def consolidate_industries(df: pd.DataFrame):
-        """Returns a dataframe that has the 12 consolidated industries we want"""
+        '''Returns a dataframe that has the 12 consolidated industries we want'''
         df["Logistics"] = df["Wholesale Trade"] + df["Transportation and Warehousing"] + df["Utilities"]
         df["FIRE"] = df["Finance and Insurance"] + df["Real Estate and Rental and Leasing"]
         df["Professional and Business Services"] = df["Professional, Scientific, and Technical Services"] + df["Management of Companies and Enterprises"] + df["Administrative and Support and Waste Management and Remediation Services"]
@@ -137,7 +137,8 @@ def employment_composition(city: str, data_path: str):
     empl_share_df = empl_share_df.drop(index='Total Employment ')
     empl_share_df = empl_share_df.sort_values('Employment Share', ascending=True)
     plots(empl_share_df)
-employment_composition('Cathedral City', '../data-automation/data/CV_EMPL.csv')
+#employment_composition('Cathedral City', '../data-automation/data/CV_EMPL.csv')
+
 
 def chge_empl_share_prepk_sector(city: str, data_path: str):
     def filter_df(city: str, df):
@@ -150,10 +151,16 @@ def chge_empl_share_prepk_sector(city: str, data_path: str):
         '''
         Plots data using plotly
         '''
-        empl_data_plot = px.bar(df, x=df.index, y='Employment Share', labels={'index': 'Sector'}, text='Employment Share')
-        empl_data_plot.update_traces(textposition='outside')
-        empl_data_plot.show()
-        return empl_data_plot
+        fig = px.bar(graph_df, x="INDUSTRY",
+                 y=["remaining", "recovery", "netgain"],
+                 title=f"Change  (Absolute Changes)")
+        fig.show()
+    
+        fig2 = px.bar(graph_df, x="INDUSTRY",
+                 y=["remaining_perc", "recovery_perc", "netgain_perc"],
+                 title=f"Peak-to-trough employment (Percent Changes)")
+    
+        fig2.show()
     def consolidate_industries(df: pd.DataFrame):
         """Returns a dataframe that has the 12 consolidated industries we want"""
         df["Logistics"] = df["Wholesale Trade"] + df["Transportation and Warehousing"] + df["Utilities"]
@@ -178,9 +185,162 @@ def chge_empl_share_prepk_sector(city: str, data_path: str):
     empl_data = empl_data.fillna(0)
     empl_data = filter_df(city, empl_data)
     empl_data = consolidate_industries(empl_data)
+    empl_data = empl_data.set_index('DATE')
     empl_data['Total Employment'] = empl_data.iloc[:, 2:].sum(axis=1)
     for sector in empl_data.columns[2:]:
         empl_data[sector + ' '] = empl_data[sector]/empl_data['Total Employment']
-    empl_share_df = pd.DataFrame()
-    empl_share_df.index = empl_data.columns[14:]
-    empl_share_df['Employment Share'] = empl_data.iloc[0, 14:]
+    empl_share_df = empl_data.drop(columns=empl_data.columns[2:14])
+    max_frame = empl_share_df.loc["2013-01-01":"2020-03-01", :]
+    min_frame = empl_share_df.loc["2020-03-01":, :]
+    industryCols = empl_share_df.columns[2:]
+
+    now = empl_share_df.iloc[-1, :]
+
+    maxes = []
+    mins = []
+    now_vals = []
+    for col in industryCols:
+        maxes.append(max_frame[col].max())
+        mins.append(min_frame[col].min())
+        now_vals.append(now[col])
+
+    graph_df = pd.DataFrame({
+        "INDUSTRY": industryCols,
+        "MAX": maxes,
+        "MIN": mins,
+        "NOW": now_vals
+    })
+
+    remaining = []
+    recovery = []
+    netgain = []
+    
+    for idx, item in graph_df.iterrows():
+        if item["NOW"] < item["MAX"]:
+            remaining.append(item["NOW"] - item["MAX"])
+            netgain.append(0)
+            recovery.append(item["MIN"] - item["NOW"])
+        else:
+            remaining.append(0)
+            netgain.append(item["NOW"] - item["MAX"])
+            recovery.append(item["MIN"] - item["MAX"])
+    
+    graph_df["remaining"] = remaining
+    graph_df["recovery"] = recovery
+    graph_df["netgain"] = netgain
+    
+    graph_df["remaining_perc"] = graph_df["remaining"] / graph_df["MAX"]
+    graph_df["recovery_perc"] = graph_df["recovery"] / graph_df["MAX"]
+    graph_df["netgain_perc"] = graph_df["netgain"] / graph_df["MAX"]
+    
+    
+    #print(empl_data)
+    plots(graph_df)
+#chge_empl_share_prepk_sector('Cathedral City', '../data-automation/data/CV_EMPL.csv')
+
+def peak_to_trough_empl(data: str, city: str):
+
+    def consolidate_industries(df: pd.DataFrame):
+        """Returns a dataframe that has the 12 consolidated industries we want"""
+        df["Logistics"] = df["Wholesale Trade"] + df["Transportation and Warehousing"] + df["Utilities"]
+        df["FIRE"] = df["Finance and Insurance"] + df["Real Estate and Rental and Leasing"]
+        df["Professional and Business Services"] = df["Professional, Scientific, and Technical Services"] + df["Management of Companies and Enterprises"] + df["Administrative and Support and Waste Management and Remediation Services"]
+        df["Leisure and Hospitality"] = df["Arts, Entertainment, and Recreation"] + df["Accommodation and Food Services"]
+        df["Education and Health Services"] = df["Educational Services"] + df["Health Care and Social Assistance"]
+        df["Government"] = df["Federal Government"] + df["State Government"] + df["Local Government"]
+        df["Mining and Natural Resources"] = df["Mining, Quarrying, and Oil and Gas Extraction"] + df["Agriculture, Forestry, Fishing and Hunting"]
+        
+        cols_to_select = ["DATE", "City", "Logistics", "Construction", "Manufacturing",
+                        "Retail Trade", "Information", "FIRE",
+                        "Professional and Business Services", "Leisure and Hospitality",
+                        "Education and Health Services", "Government",
+                        "Mining and Natural Resources", "Total"]
+        
+        res = df[cols_to_select]
+        return res
+    # Read in the employment data
+    empl = pd.read_csv(data, na_values=["***", ".", "NA"])
+    empl['DATE'] = empl['DATE'].str.pad(width=6, side="left", fillchar="0")
+    empl["Total"] = empl.loc[:, "Agriculture, Forestry, Fishing and Hunting":].sum(axis=1)
+    empl['DATE'] = pd.to_datetime(empl.DATE, format='%y-%b')
+    empl["City"] = empl["City"].str.lower()
+    empl = empl.fillna(0)
+
+    empl_c = consolidate_industries(empl)
+    # Filter for the city
+    city = empl[empl["City"] == city.lower()]
+    
+    # Set the index to be a datetime index and get rid of empty rows
+    city = city.set_index("DATE")
+    city = city[city["Total"] != 0]
+    
+    # Get the present value for each industry and note which 
+    now = city.iloc[-1, :]
+    
+    # Get rid of any industries that have no present workers
+    cols_to_drop = []
+    for idx, item in now.iteritems():
+        try:
+            item = int(item)
+            if item == 0:
+                cols_to_drop.append(idx)
+        except ValueError:
+            continue
+    city = city.drop(columns=cols_to_drop + ["Total"])
+    industryCols = city.loc[:, "Logistics":].columns
+    
+    # Get dataframes to calculate the max and min for each industry
+    max_frame = city.loc["2013-01-01":"2020-03-01", :]
+    min_frame = city.loc["2020-03-01":, :]
+    
+    maxes = []
+    mins = []
+    now_vals = []
+    for col in industryCols:
+        maxes.append(max_frame[col].max())
+        mins.append(min_frame[col].min())
+        now_vals.append(now[col])
+    
+    graph_df = pd.DataFrame({
+        "INDUSTRY": industryCols,
+        "MAX": maxes,
+        "MIN": mins,
+        "NOW": now_vals
+    })
+    
+    # Add columns to graph_df for the plot
+    remaining = []
+    recovery = []
+    netgain = []
+    
+    for idx, item in graph_df.iterrows():
+        if item["NOW"] < item["MAX"]:
+            remaining.append(item["NOW"] - item["MAX"])
+            netgain.append(0)
+            recovery.append(item["MIN"] - item["NOW"])
+        else:
+            remaining.append(0)
+            netgain.append(item["NOW"] - item["MAX"])
+            recovery.append(item["MIN"] - item["MAX"])
+    
+    graph_df["remaining"] = remaining
+    graph_df["recovery"] = recovery
+    graph_df["netgain"] = netgain
+    
+    graph_df["remaining_perc"] = graph_df["remaining"] / graph_df["MAX"]
+    graph_df["recovery_perc"] = graph_df["recovery"] / graph_df["MAX"]
+    graph_df["netgain_perc"] = graph_df["netgain"] / graph_df["MAX"]
+    
+    fig = px.bar(graph_df, x="INDUSTRY",
+                 y=["remaining", "recovery", "netgain"],
+                 title=f"Peak-to-trough employment (Absolute Changes)")
+    fig.show()
+    
+    fig2 = px.bar(graph_df, x="INDUSTRY",
+                 y=["remaining_perc", "recovery_perc", "netgain_perc"],
+                 title=f"Peak-to-trough employment (Percent Changes)")
+    
+    fig2.show()
+    
+    return graph_df
+peak_to_trough_empl('../data-automation/data/CV_EMPL.csv', 'cathedral city')
