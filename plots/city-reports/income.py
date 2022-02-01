bda32cc5358f5c4e46313ee4cda0afd443c4b6c3
Abhi Uppal <abhinuv2000@gmail.com>
2022-02-01 15:24:51 -0800

    last income graph edits

--- plots/city-reports/income.py
+++ plots/city-reports/income.py
@@ -3,6 +3,7 @@
 import asyncio
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 from lowe.locations.lookup import name2fips
 from lowe.acs.ACSClient import ACSClient
@@ -26,6 +27,7 @@ acc_ter = "#e7c8ae"
 # Helper Functions
 # ------------------------------
 
+
 def _axis_line_breaks_elem(elem: str, max_chars: int = 15):
     """Adds line breaks to axis values so they can be plotted horizontally"""
     splt = elem.split(" ")
@@ -45,6 +47,7 @@ def _axis_line_breaks_elem(elem: str, max_chars: int = 15):
 
 def _axis_line_breaks(elems: list, max_chars: int = 15):
     return [*map(lambda x: _axis_line_breaks_elem(x, max_chars), elems)]
+
 
 # ------------------------------
 # Plot Generation
@@ -122,7 +125,9 @@ async def total_household_income(
     # ISOLATING TARGET CITY
     city_list = plot_df["city"].values.tolist()
     city_index = city_list.index(target_city[0 : (len(target_city)) - 3].title())
-    colors = [pri_color,] * len(cities)
+    colors = [
+        pri_color,
+    ] * len(cities)
     colors[city_index] = fund_ter
 
     fig = px.bar(
@@ -144,7 +149,7 @@ async def total_household_income(
         font_size=18,
     )
 
-    fig.update_traces(marker_color=colors, textposition='outside', width=0.5)
+    fig.update_traces(marker_color=colors, textposition="outside", width=0.5)
 
     if save_path is not None:
         fig.write_image(
@@ -216,7 +221,9 @@ async def median_household_income(
     # ISOLATING TARGET CITY
     city_list = plot_df["Type"].values.tolist()
     city_index = city_list.index(target_city[0 : (len(target_city)) - 3].title())
-    colors = [pri_color,] * len(cities)
+    colors = [
+        pri_color,
+    ] * len(cities)
     colors[city_index] = fund_ter
 
     fig = px.bar(
@@ -239,7 +246,7 @@ async def median_household_income(
         font_size=18,
     )
 
-    fig.update_traces(marker_color=colors, textposition='outside', width=0.5)
+    fig.update_traces(marker_color=colors, textposition="outside", width=0.5)
 
     if save_path is not None:
         fig.write_image(
@@ -254,7 +261,18 @@ async def median_household_income(
 
 async def household_income_by_class(
     client: ACSClient,
-    cities: list = ["coachella, ca", "desert hot springs, ca"],
+    cities: list = [
+        "cathedral city, ca",
+        "coachella, ca",
+        "desert hot springs, ca",
+        "indian wells, ca",
+        "indio, ca",
+        "la quinta, ca",
+        "palm desert, ca",
+        "palm springs, ca",
+        "rancho mirage, ca",
+    ],
+    target_city: str = "desert hot springs ca",
     year: str = "2019",
     save_path: str = None,
     img_height: int = 1080,
@@ -278,72 +296,99 @@ async def household_income_by_class(
     # NOTE: &#36; is a $ -- needed in any label with 2 dollar signs. Without this, plotly tries to render it as LaTeX (math)
     cols = {
         # old col name: new name
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Households Total": "Total Households",
         "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total Less than $10,000": "Less than $10,000",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $10,000 to $14,999": "&#36;10,000-&#36;14,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $15,000 to $24,999": "&#36;15,000-&#36;24,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $25,000 to $34,999": "&#36;25,000-&#36;34,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $35,000 to $49,999": "&#36;35,000-&#36;49,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $50,000 to $74,999": "&#36;50,000-&#36;74,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $75,000 to $99,999": "&#36;75,000-&#36;99,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $100,000 to $149,999": "&#36;100,000-&#36;149,999",
-        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $150,000 to $199,999": "&#36;150,000-&#36;199,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $10,000 to $14,999": "&#36;10,000-<br>&#36;14,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $15,000 to $24,999": "&#36;15,000-<br>&#36;24,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $25,000 to $34,999": "&#36;25,000-<br>&#36;34,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $35,000 to $49,999": "&#36;35,000-<br>&#36;49,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $50,000 to $74,999": "&#36;50,000-<br>&#36;74,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $75,000 to $99,999": "&#36;75,000-<br>&#36;99,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $100,000 to $149,999": "&#36;100,000-<br>&#36;149,999",
+        "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $150,000 to $199,999": "&#36;150,000-<br>&#36;199,999",
         "INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS) Estimate Nonfamily households Total $200,000 or more": "$200,000+",
     }
 
-    loc_dicts = [{"city": city} for city in cities]
-    loc_fips = [*map(name2fips, loc_dicts)]
+    loc_dicts_cv = [{"city": city} for city in cities]
+    loc_fips_cv = [*map(name2fips, loc_dicts_cv)]
 
-    resp = await client.get_acs(
-        vars=["S1901"], start_year=year, end_year=year, estimate="5", location=loc_fips
+    cv = await client.get_acs(
+        vars=["S1901"],
+        start_year=year,
+        end_year=year,
+        estimate="5",
+        location=loc_fips_cv,
     )
 
-    col_sub = [*list(cols.keys()), "state", "city"]
-    resp = resp[col_sub]
-    resp = resp.rename(columns=cols)
+    col_sub = [*list(cols.keys()), "state", "city", "location_key"]
+    cv = cv[col_sub]
+    cv = cv.rename(columns=cols)
 
-    # Following is unique to each section
+    target_city_df = cv[cv["location_key"].str.lower().str.contains(target_city)]
 
-    categ = resp.columns[0:10].astype(str)
-    categ = _axis_line_breaks(categ, 10)
-    bar_value = resp.iloc[1][0:10].astype(str).astype(float)
-    line_value = resp.iloc[0][0:10].astype(str).astype(float)
+    # Calculate the income distribution for Coachella Valley
 
-    bar_df = pd.DataFrame({"Type": categ, "Percentage": bar_value})
-    line_df = pd.DataFrame({"Type": categ, "Percentage": line_value})
+    class_cols = list(cv.loc[:, "Less than $10,000":"$200,000+"].columns)
+    class_cols_num = []
 
-    fig = px.line(
-        line_df,
-        x="Type",
-        y="Percentage",
-        color=px.Constant("Coachella Valley"),
-        color_discrete_sequence=[sec_color],
+    for col in class_cols:
+        colname = f"{col}_num"
+        cv[colname] = cv[col].astype(float) * cv["Total Households"].astype(int) / 100
+        class_cols_num.append(colname)
+
+    # Sum together the number columns
+    cv = cv[class_cols_num + ["Total Households"]]
+    cv["Total Households"] = cv["Total Households"].astype(int)
+    cv = cv.sum(axis=0)
+
+    for col in class_cols_num:
+        colname = col.split("_")[0]
+        cv[colname] = cv[col] / cv["Total Households"]
+
+    cv = cv[class_cols]
+    xaxis = list(cv.index)
+    yvals_target = [float(target_city_df.loc[:, x].iloc[0]) / 100 for x in xaxis]
+    yvals_cv = list(cv)
+    cityname_split = target_city.split(" ")[:-1]
+    cityname = " ".join(cityname_split).title()
+
+    # Plot!
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Bar(
+            x=xaxis,
+            y=yvals_target,
+            name=cityname,
+            text=[*map(lambda x: f"{x * 100:.1f}%", yvals_target)],
+            textposition="outside",
+            textfont_size=16,
+            marker_color=fund_pri,
+        )
     )
-    fig.add_bar(
-        x=bar_df["Type"],
-        y=bar_df["Percentage"],
-        name="Desert Hot Spring",
-        text=bar_df["Percentage"].apply(lambda x: "{0:,.1f}%".format(x)),
+
+    fig.add_trace(
+        go.Scatter(
+            x=xaxis,
+            y=yvals_cv,
+            name="Coachella Valley",
+            marker_color=acc_ter,
+            marker_size=8,
+            line=dict(width=3, color=acc_ter),
+        )
     )
 
     fig.update_layout(
         font_family="Glacial Indifference",
         font_color="black",
-        title_font_family="Glacial Indifference",
-        title_font_color="black",
-        legend_title_font_color="black",
-        legend_title_text="Region",
-        template="plotly_white",
-        xaxis_title="City",
-        yaxis_title="Percent of Households",
         font_size=18,
+        yaxis_title="Percentage of Households",
+        legend_title_font_color="black",
+        template="plotly_white",
+        xaxis_title="",
+        legend=dict(x=0.5, orientation="h", xanchor="center"),
     )
-
-    fig.update_traces(marker_color=pri_color, line=dict(width=3))
-
-    if save_path is not None:
-        fig.write_image(
-            save_path, height=img_height, width=img_width, scale=scale, format="png"
-        )
 
     return fig
 
