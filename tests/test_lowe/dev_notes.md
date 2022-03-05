1437ef18fa6351fdac3e4363d568106b5b3fac09
aaronxie <aaron.xie2024@gmail.com>
2022-03-04 19:07:28 -0800

    table 1 of 3 for 5 year est.

--- tests/test_lowe/dev_notes.md
+++ tests/test_lowe/dev_notes.md
@@ -1,23 +1,16 @@
+`python3 -m pytest tests/test_lowe/test_acs/test_acsclient.py`
 
-Test API Wrapper (initalize client, pull data from different soruces (eg. ACS tables) and ensure data equals what is expected. Pick random cells of table and compare between manual access via website vs wrapper). 
+## Description of Test
 
-eg.: https://data.census.gov/cedsci/table?q=S1901%20Coachella%20Valley 
-vs 
-```
-raw_resp = await client.get_acs(vars = 'S1901',
-                     start_year=2019,
-                     end_year=2019,
-                     location='06065',
-                     estimate="5") 
-```
+- Check 3 tables with 1 and 5 years (6 tables)
+- For each table check 3 different values
 
-test command
-`pytest lowe/test/test_acesclient.py`
+Common Tables Used in Lowe
+- S0801
+- S2801
+- S1901
+- S1501
+- DP05
 
----
+Compare client pull with manual check eg. at https://data.census.gov/cedsci/table?q=coachella,%20ca%20S1501 
 
-each table has:
-
-5 year estimates, try 3 tables (diff year for each table), for each of table check 3 different values.
-
-and repeat for 1 year
--- tests/test_lowe/test_acs/test_acsclient.py
+++ tests/test_lowe/test_acs/test_acsclient.py
@@ -2,31 +2,85 @@ import pytest
 from lowe.acs.ACSClient import ACSClient
 from lowe.locations.lookup import search, name2fips, fips2name
 
-def test_add():
-    assert 1+1 == 2
-
 @pytest.mark.asyncio
-async def test_overall():
-    client = ACSClient()
-    await client.initialize()
-    fip_code = search('cathedral city, ca', codetype="city", search_on="name").iloc[0][1]
+class TestSearch_and_Get:
+    """
+    tests:
+    - ACSClient Initialization
+    - client.get_acs()
 
-    loc1 = {
-    "state": "06",
-    "city": "14260"
-    }
+    needs working:
+    - lowe.locations.lookup search()
+    """
 
-    loc2 = {"city": "0636434"}
+    async def test_add(self):
+        assert 1+1 == 2
 
-    locs = [loc1, loc2] 
+    async def test_5year_s2801_1(self):
+        client = ACSClient()
+        await client.initialize()
+        fip_code = search('cathedral city, ca', codetype="city", search_on="name").iloc[0][1]
 
-    raw_resp = await client.get_acs(vars = ['S1701'],
-                     start_year='2018',
-                     end_year='2018',
-                     location=locs,
-                     estimate="5")
-    print(raw_resp)
-    assert False
+        # fip_code2 = search('coachella, ca', codetype="city", search_on="name").iloc[0][1]
 
+        locs = [{'city': fip_code}]
+        
+        resp = await client.get_acs(vars = ['S2801'],
+                        start_year='2019',
+                        end_year='2019',
+                        location=locs,
+                        estimate="5")
+        col_name = "TYPES OF COMPUTERS AND INTERNET SUBSCRIPTIONS Estimate Percent Total households TYPE OF INTERNET SUBSCRIPTIONS With an Internet subscription: Broadband of any type"
+        col_sub = [col_name, 'state', 'city']
+        if len(resp[col_sub]) != 1:
+            assert False
 
+        value = float(resp[col_sub][col_name].iloc[0])
 
+        assert value == 82.2
+
+    async def test_5year_s2801_2(self):
+        client = ACSClient()
+        await client.initialize()
+        fip_code = search('cathedral city, ca', codetype="city", search_on="name").iloc[0][1]
+
+        # fip_code2 = search('coachella, ca', codetype="city", search_on="name").iloc[0][1]
+
+        locs = [{'city': fip_code}]
+        
+        resp = await client.get_acs(vars = ['S2801'],
+                        start_year='2019',
+                        end_year='2019',
+                        location=locs,
+                        estimate="5")
+        col_name = "TYPES OF COMPUTERS AND INTERNET SUBSCRIPTIONS Estimate Percent Total households TYPES OF COMPUTER Has one or more types of computing devices: Desktop or laptop"
+        col_sub = [col_name, 'state', 'city']
+        if len(resp[col_sub]) != 1:
+            assert False
+
+        value = float(resp[col_sub][col_name].iloc[0])
+
+        assert value == 75.3
+
+    async def test_5year_s2801_3(self):
+        client = ACSClient()
+        await client.initialize()
+        # fip_code = search('cathedral city, ca', codetype="city", search_on="name").iloc[0][1]
+
+        fip_code2 = search('coachella, ca', codetype="city", search_on="name").iloc[0][1]
+
+        locs = [{'city': fip_code2}]
+        
+        resp = await client.get_acs(vars = ['S2801'],
+                        start_year='2019',
+                        end_year='2019',
+                        location=locs,
+                        estimate="5")
+        col_name = "TYPES OF COMPUTERS AND INTERNET SUBSCRIPTIONS Estimate Total Total households TYPE OF INTERNET SUBSCRIPTIONS With an Internet subscription: Broadband of any type"
+        col_sub = [col_name, 'state', 'city']
+        if len(resp[col_sub]) != 1:
+            assert False
+
+        value = float(resp[col_sub][col_name].iloc[0])
+
+        assert value == 11273
